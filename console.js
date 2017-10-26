16d9d10d6627b7d59f80af0c034a5ddf97e104b3
Martin Kleppe <kleppe@ubilabs.net>
2017-10-26 22:42:59 +0200

    use $.map for Twitter

--- console.js
+++ console.js
@@ -1,6 +1,2 @@
-for(S=12,c=A=66,z=[i=132];i--;)z[i]=i%S?5:-1;X=a=>(D=[S,1,-S,-1]).map((b,e,j)=>1<z[b=a+2*j[3&e+i]]&&(z[b]=z[(a+b)/2]=0,i=9*Math.random(),X(b))),X(S+2),F=0
-
-while(!F){c==14?F=c=A:F||(z[c]=(z[c]+1)%4),5>z[n=c+D[z[c]]]&&(c=n),F||(Z=z.slice``),Z[c]=4};
-
-""+Z.map(b=>"    +X"[b]||`
+for(S=12,c=A=66,z=[i=132];i--;)z[i]=i%S?5:-1;X=a=>(D=[S,1,-S,-1]).map((b,e,j)=>1<z[b=a+2*j[3&e+i]]&&(z[b]=z[(a+b)/2]=0,i=9*Math.random(),X(b))),X(S+2),F=0;while(F<2){c==14?c=++F&&A:F||(z[c]=(z[c]+1)%4),5>z[n=c+D[z[c]]]&&(c=n),F||($=z.slice``),$[c]=4};""+$.map(b=>"    .#"[b]||`
 `)
