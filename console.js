a727bf150ecfcc5d18fc93b64f744b490e517edf
Martin Kleppe <kleppe@ubilabs.net>
2017-10-27 09:30:41 +0200

    split into 4 parts (use \n instead of ;)
    1. init array
    2. render maze
    3. solve maze
    4. render result

--- console.js
+++ console.js
@@ -1,2 +1,5 @@
-for(z=[c=66,i=132];i--;)D=[12,1,-12,z[i]=i%12?5:-1];X=a=>D.map((b,e)=>1<z[b=a+2*D[3&e+i]]&&X(b,i=9*Math.random(z[b]=z[(a+b)/2]=F=0))),X(14);while(F<2){c^14?z[c]=z[c]+1-F&3:c=++F&&66,5>z[n=c+D[z[c]]]&&(c=n),F?0:$=z.slice``,$[c]=4};``+$.map(b=>`
+for(z=[c=66,i=132];i--;)D=[12,1,-12,z[i]=i%12?5:-1]
+X=a=>D.map((b,e)=>1<z[b=a+2*D[3&e+i]]&&X(b,i=9*Math.random(z[b]=z[(a+b)/2]=F=0))),X(14)
+while(F<2){c^14?z[c]=z[c]+1-F&3:c=++F&&66,5>z[n=c+D[z[c]]]&&(c=n),F?0:$=z.slice``,$[c]=4}
+``+$.map(b=>`
     oW`[++b])
