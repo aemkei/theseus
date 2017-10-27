e0fa399d5fe72e6bdc599863da1a0ab7d61b99b5
Martin Kleppe <kleppe@ubilabs.net>
2017-10-27 09:25:58 +0200

    heavy refactoring
    * moving the `i=9*...` as a second argument of the call to `X(b)`,
    * replacing the `c==14?foo:bar` by `c^14?bar:foo`
    * removing `A=` and `A` to use `66` directly
    * moving the initialization of `D` in the increment statement of the `for` loop
    * initializing `F` as the cells are set to 0 when creating the maze
    * using `D` in the `D.map((b,e,j)=>...` instead of using the third argument of the map function
    * Moving the initializing of `D` once more to initialize `z` in place of the `-1` offset in `D`
    * Exploiting the fact that `F` is only `0` or `1` to replace the `F?0:z[c]=z[c]+1&3` by `z[c]=z[c]+1-F&3`
    
    -16 by @veu an @p01

--- console.js
+++ console.js
@@ -1,2 +1,2 @@
-for(c=A=66,z=[i=132];i--;)z[i]=i%12?5:-1;X=a=>(D=[12,1,-12,-1]).map((b,e,j)=>1<z[b=a+2*j[3&e+i]]&&(z[b]=z[(a+b)/2]=0,i=9*Math.random(),X(b))),X(14),F=0;while(F<2){c==14?c=++F&&A:F||(z[c]=z[c]+1&3),5>z[n=c+D[z[c]]]&&(c=n),F||($=z.slice``),$[c]=4};""+$.map(b=>`
-oW`[++b])
+for(z=[c=66,i=132];i--;)D=[12,1,-12,z[i]=i%12?5:-1];X=a=>D.map((b,e)=>1<z[b=a+2*D[3&e+i]]&&X(b,i=9*Math.random(z[b]=z[(a+b)/2]=F=0))),X(14);while(F<2){c^14?z[c]=z[c]+1-F&3:c=++F&&66,5>z[n=c+D[z[c]]]&&(c=n),F?0:$=z.slice``,$[c]=4};``+$.map(b=>`
+    oW`[++b])
